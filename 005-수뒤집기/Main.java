82bf075055b809532c0956a765bb17e51a371386
FireGirlJimin <njmin8@naver.com>
2023-09-08 10:06:16 +0900

    soltion 추가

--- 005-수뒤집기/Main.java
+++ 005-수뒤집기/Main.java
@@ -1,14 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     private static double correctCount = 0;
 
     private static final String[][] TESTCASES = new String[][] {
-            {"123", "123", "246"},
-            {"1000", "1", "2"},
-            {"456", "789", "1461"},
-            {"5", "5", "1"},
-            {"11112", "54321", "65433"},
-            {"3829", "1300", "4139"}
+            { "123", "123", "246" },
+            { "1000", "1", "2" },
+            { "456", "789", "1461" },
+            { "5", "5", "1" },
+            { "11112", "54321", "65433" },
+            { "3829", "1300", "4139" }
     };
 
     public static void main(String[] args) {
@@ -28,6 +31,37 @@ public class Main {
         return res;
     }
 
+    public static String solution(int first, int second) {
+        int firstInt = Integer.parseInt(reverse(first + ""));
+        int secondInt = Integer.parseInt(reverse(second + ""));
 
-    // solution
+        int resultInt = firstInt + secondInt;
+        String result = removeZero(resultInt);
+        return result;
+    }
+
+    public static String reverse(String input) {
+        char[] inputArr = input.toCharArray();
+        char[] newInputArr = new char[inputArr.length];
+
+        for (int i = 0; i < inputArr.length; i++) {
+            newInputArr[i] = inputArr[input.length() - 1 - i];
+        }
+
+        String revResult = new String(newInputArr);
+        return revResult;
+    }
+
+    public static String removeZero(int input) {
+        if (input % 10 == 0) {
+            String newInput = reverse(input + "");
+            int newResult = Integer.parseInt(newInput);
+            return newResult + "";
+
+        } else {
+            String result = reverse(input + "");
+            return result;
+        }
+    }
+
 }
